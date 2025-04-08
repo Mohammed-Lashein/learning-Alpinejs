762e45309216dd98df90a1b8d39459ee2b22c540
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-04-08 15:22:19 +0200

    feat: implement clicking outside of the dropdown closes it

--- src/data/dropdownActions.ts
+++ src/data/dropdownActions.ts
@@ -1,10 +1,13 @@
+import { AlpineComponent } from "alpinejs"
+
 type dropdownActionsType = {
   open: boolean,
   currentAction: null | string,
   onDropdownElementClick: Function
+  // init: Function
 }
 
-export function dropdownActions(): dropdownActionsType {
+export function dropdownActions(): AlpineComponent<dropdownActionsType> {
   return {
     open: false,
     currentAction: null,
@@ -14,6 +17,6 @@ export function dropdownActions(): dropdownActionsType {
     this.open = !this.open
   console.log(this.open);
   console.log(this.currentAction)
-}
+},
   }
 }
