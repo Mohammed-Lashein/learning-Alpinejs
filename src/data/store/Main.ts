3bc23b8f939d663db115cddf4b4bb72baecb822e
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-04-17 17:22:35 +0200

    feat: add isMobileproperty in the store in  Main.ts

--- src/data/store/Main.ts
+++ src/data/store/Main.ts
@@ -1,5 +1,9 @@
-export function Main() {
-  return {
-    
-  }
+import { isMobile } from "../../utils/device"
+
+type MainStoreType = {
+  isMobile: boolean
+}
+// Note that the below is a type assertion and not a generic type
+export const Main = <MainStoreType>{
+    isMobile: isMobile()
 }
